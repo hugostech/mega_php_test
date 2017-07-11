4dfb652504e63b63d8d2f5125354878370a00d87
Hugo <hugowangchn@gmail.com>
2017-07-11 12:24:30 +1200

    config files

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hugo
+ * Date: 11/07/17
+ * Time: 12:23 PM
+ */
+
+$config['google_api_key'] = 'AIzaSyBxdt9V48rh0tSe0vkMEQ-bXN6w06wqJfw';
---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hugo
+ * Date: 11/07/17
+ * Time: 12:22 PM
+ */
+$config['db_host'] = 'localhost';
+
+$config['db_username'] = 'homestead';
+
+$config['db_password'] = 'secret';
+
+$config['db_database_name'] = 'mega_test';
