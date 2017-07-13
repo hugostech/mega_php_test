e48864c9cac5acc5da5961bd1353e8f14c695fbc
Hugo <hugowangchn@gmail.com>
2017-07-13 14:31:49 +1200

    add comments

--- controller/AppController.php
+++ controller/AppController.php
@@ -19,6 +19,17 @@ class AppController extends Controller
 {
     use \utility;
 
+    /*
+     * display the index page*/
+    public function index()
+    {
+        $data = $this->generateTranslatedData();
+
+        return $this->view('index', compact('data'));
+    }
+
+    /*
+     * run the program, grab string and translate it.*/
     public function run($request)
     {
         if (isset($request['original_text'])&&!is_null($request['original_text'])){
@@ -32,157 +43,7 @@ class AppController extends Controller
     }
 
     /*
-     *  list original text, langcode and translated text table set data*/
-    public function randomTranslate($string, $original_id)
-    {
-        $tags = $this->detectTags($string);
-        $googleList = $this->getGoogleSupportList();
-        shuffle($googleList);
-        $translated = new Translated_strings();
-        $broken_string = new Broken_strings();
-        foreach (range(0, 4) as $i) {
-            $translated_string = $this->translate($string, $googleList[$i]);
-            if ($this->matchTags($translated_string, $tags)) {
-                $translated->save_translatedtext($translated_string, $original_id, $googleList[$i]);
-            } else {
-                $broken_string->save_brokentranslatedtext($translated_string,$original_id,$googleList[$i],json_encode($tags));
-            }
-
-        }
-    }
-
-    public function detectTags($string)
-    {
-
-        //ignore %1
-        preg_match_all('/%\d*\b/', $string, $matches1);
-
-        //ignore tag []
-        preg_match_all('/\[.*?\]/', $string, $matches2);
-
-        $result = array_merge_recursive($matches1[0], $matches2[0]);
-
-        return array_unique($result);
-    }
-
-    public function getGoogleSupportList()
-    {
-        global $config;
-        $url = 'https://translation.googleapis.com/language/translate/v2/languages?key=' . $config['google_api_key'];
-        $data = json_decode($this->getContent($url), true);
-        $data = $data['data']['languages'];
-        $list = array();
-
-        foreach ($data as $line) {
-            $list[] = $line['language'];
-        }
-        return $list;
-    }
-
-    public function translate($string, $langcode)
-    {
-
-
-        $params = [
-            'source' => 'en',
-            'target' => $langcode,
-            'q' => $string,
-            'format' => 'text'
-        ];
-        global $config;
-        $url = 'https://www.googleapis.com/language/translate/v2?key=' . $config['google_api_key'];
-        $result = $this->getContentByPostData($params, $url);
-        $result = json_decode($result, true);
-        $result = html_entity_decode($result['data']['translations'][0]['translatedText']);
-
-        return $result;
-    }
-
-    /*
-     * check the tags if being changed*/
-
-    public function matchTags($string, $tags)
-    {
-        foreach ($tags as $tag) {
-            $tem = str_replace($tag, '', $string);
-            if ($tem == $string) {
-                return false;
-            }
-            $string = $tem;
-        }
-        return true;
-    }
-
-    /*
-     * find the tags in string*/
-
-    public function index()
-    {
-        $data = $this->generateTranslatedData();
-
-        return $this->view('index', compact('data'));
-    }
-
-    /*
-     * translate string by using google api*/
-
-    public function generateTranslatedData()
-    {
-        $format = <<<TABLE
-                <tr>
-                    <form method="post" action="?route=update">
-                        <td>%s</td>
-                        <td>%s</td>
-                        <td><input type="text" name="translatedtext" value="%s" class="form-control"></td>
-                        <td>
-
-                            <input type="hidden" name="originalstringid" value=%u>
-                            <input type="hidden" name="translated_string_id" value=%u>
-                            <input type="hidden" name="langcode" value="%s">
-                            <input type="submit" value="Edit" class="btn btn-sm btn-primary">
-                        </td>
-                    </form>
-
-                </tr>
-TABLE;
-        $broken_format = <<<TABLE
-                <tr>
-                    <form method="post" action="?route=brokenFix">
-                        <td>%s</td>
-                        <td class="danger">%s</td>
-                        <td><input type="text" name="translatedtext" value="%s" class="form-control"></td>
-                        <td>
-
-                            <input type="hidden" name="originalstringid" value=%u>
-                            <input type="hidden" name="broken_string_id" value=%u>
-                            <input type="hidden" name="langcode" value="%s">
-                            <input type="submit" value="Fix" class="btn btn-sm btn-primary">
-                        </td>
-                    </form>
-
-                </tr>
-TABLE;
-
-        $data = '';
-        $englist_strings = new English_strings();
-        $strings = $englist_strings->all();
-        foreach ($strings as $string) {
-            $tanslated_strings = $englist_strings->translated_strings($string['id']);
-            foreach ($tanslated_strings as $tanslated_string) {
-                $data .= sprintf($format, $string['text'], $tanslated_string['langcode'], $tanslated_string['translatedtext'], $string['id'], $tanslated_string['id'], $tanslated_string['langcode']);
-            }
-            $broken_strings = $englist_strings->broken_string($string['id']);
-
-            foreach ($broken_strings as $broken_string) {
-                $data .= sprintf($broken_format, $string['text'], $broken_string['langcode'], $broken_string['brokentext'], $string['id'],$broken_string['id'], $broken_string['langcode']);
-            }
-        }
-        return $data;
-    }
-
-    /*
-     * grab google translate support language*/
-
+    * update tanslated string item*/
     public function update($request)
     {
         $translated_string = new Translated_strings();
@@ -212,4 +73,151 @@ TABLE;
         }
         return $this->index();
     }
+
+    /*
+     *  random translate string to five different languages*/
+    public function randomTranslate($string, $original_id)
+    {
+        $tags = $this->detectTags($string);
+        $googleList = $this->getGoogleSupportList();
+        shuffle($googleList);
+        $translated = new Translated_strings();
+        $broken_string = new Broken_strings();
+        foreach (range(0, 4) as $i) {
+            $translated_string = $this->translate($string, $googleList[$i]);
+            if ($this->matchTags($translated_string, $tags)) {
+                $translated->save_translatedtext($translated_string, $original_id, $googleList[$i]);
+            } else {
+                $broken_string->save_brokentranslatedtext($translated_string,$original_id,$googleList[$i],json_encode($tags));
+            }
+
+        }
+    }
+
+
+
+    /*
+     * grab google support language list*/
+    public function getGoogleSupportList()
+    {
+        global $config;
+        $url = 'https://translation.googleapis.com/language/translate/v2/languages?key=' . $config['google_api_key'];
+        $data = json_decode($this->getContent($url), true);
+        $data = $data['data']['languages'];
+        $list = array();
+
+        foreach ($data as $line) {
+            $list[] = $line['language'];
+        }
+        return $list;
+    }
+
+    /*
+     * translate string to specific language*/
+    public function translate($string, $langcode)
+    {
+
+
+        $params = [
+            'source' => 'en',
+            'target' => $langcode,
+            'q' => $string,
+            'format' => 'text'
+        ];
+        global $config;
+        $url = 'https://www.googleapis.com/language/translate/v2?key=' . $config['google_api_key'];
+        $result = $this->getContentByPostData($params, $url);
+        $result = json_decode($result, true);
+        $result = html_entity_decode($result['data']['translations'][0]['translatedText']);
+
+        return $result;
+    }
+
+    /*
+     * check the tags if being changed*/
+    public function matchTags($string, $tags)
+    {
+        foreach ($tags as $tag) {
+            $tem = str_replace($tag, '', $string);
+            if ($tem == $string) {
+                return false;
+            }
+            $string = $tem;
+        }
+        return true;
+    }
+
+    /*
+     * find the tags in string*/
+    public function detectTags($string)
+    {
+
+        //ignore %1
+        preg_match_all('/%\d*\b/', $string, $matches1);
+
+        //ignore tag []
+        preg_match_all('/\[.*?\]/', $string, $matches2);
+
+        $result = array_merge_recursive($matches1[0], $matches2[0]);
+
+        return array_unique($result);
+    }
+
+    /*
+     * list tanslated string and broken string*/
+    public function generateTranslatedData()
+    {
+        $format = <<<TABLE
+                <tr>
+                    <form method="post" action="?route=update">
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td><input type="text" name="translatedtext" value="%s" class="form-control"></td>
+                        <td>
+
+                            <input type="hidden" name="originalstringid" value=%u>
+                            <input type="hidden" name="translated_string_id" value=%u>
+                            <input type="hidden" name="langcode" value="%s">
+                            <input type="submit" value="Edit" class="btn btn-sm btn-primary">
+                        </td>
+                    </form>
+
+                </tr>
+TABLE;
+        $broken_format = <<<TABLE
+                <tr>
+                    <form method="post" action="?route=brokenFix">
+                        <td>%s</td>
+                        <td class="danger">%s('broken tags')</td>
+                        <td><input type="text" name="translatedtext" value="%s" class="form-control"></td>
+                        <td>
+
+                            <input type="hidden" name="originalstringid" value=%u>
+                            <input type="hidden" name="broken_string_id" value=%u>
+                            <input type="hidden" name="langcode" value="%s">
+                            <input type="submit" value="Fix" class="btn btn-sm btn-danger">
+                        </td>
+                    </form>
+
+                </tr>
+TABLE;
+
+        $data = '';
+        $englist_strings = new English_strings();
+        $strings = $englist_strings->all();
+        foreach ($strings as $string) {
+            $tanslated_strings = $englist_strings->translated_strings($string['id']);
+            foreach ($tanslated_strings as $tanslated_string) {
+                $data .= sprintf($format, $string['text'], $tanslated_string['langcode'], $tanslated_string['translatedtext'], $string['id'], $tanslated_string['id'], $tanslated_string['langcode']);
+            }
+            $broken_strings = $englist_strings->broken_string($string['id']);
+
+            foreach ($broken_strings as $broken_string) {
+                $data .= sprintf($broken_format, $string['text'], $broken_string['langcode'], $broken_string['brokentext'], $string['id'],$broken_string['id'], $broken_string['langcode']);
+            }
+        }
+        return $data;
+    }
+
+
 }
